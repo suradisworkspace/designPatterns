f289e6286914bc7f43914c7448eefec4f4b151d7
suradisworkspace <suradis.workspace@gmail.com>
2024-09-22 19:23:51 +0700

    feat: add docs

--- patterns/structural/decorator/Analytics.ts
+++ patterns/structural/decorator/Analytics.ts
@@ -1,26 +1,60 @@
+/**
+ * @abstract
+ * @class Analytics
+ * @constructor
+ */
 class Analytics {
-  id: string;
+  private _id: string;
+  /**
+   * Analytics constructor
+   * @param {string} id - User id
+   */
   constructor(id: string) {
-    this.id = id;
+    if (this.constructor === Analytics) {
+      throw new Error("Analytics is abstract class. cant be instantiate!");
+    }
+    this._id = id;
   }
   getId() {
-    return this.id;
+    return this._id;
   }
-  //   just interface
-  send() {}
+
+  /** @abstract */
+  send() {
+    throw new Error(
+      `Method 'send()' must be implemented in '${this.constructor.name}'`
+    );
+  }
 }
 
+/**
+ * @class BaseAnalytics
+ * @extends {Analytics}
+ */
 export class BaseAnalytics extends Analytics {
+  /**
+   * Analytics constructor
+   * @param {string} id - User id
+   */
   constructor(id: string) {
     super(id);
   }
   send() {
-    console.log(`log ${this.id} into local storage`);
+    console.log(`log ${this.getId()} into local storage`);
   }
 }
 
+/**
+ * @class AnalyticsDecorator
+ * @constructor
+ * @extends {Analytics}
+ */
 class AnalyticsDecorator extends Analytics {
-  analytics: any;
+  analytics: Analytics;
+  /**
+   *
+   * @param {Analytics} analytics - any analytics class
+   */
   constructor(analytics: Analytics) {
     super(analytics.getId());
     this.analytics = analytics;
@@ -30,22 +64,32 @@ class AnalyticsDecorator extends Analytics {
   }
 }
 
+/**
+ * @class GoogleAnalytics
+ * @constructor
+ * @extends {AnalyticsDecorator}
+ */
 export class GoogleAnalytics extends AnalyticsDecorator {
   constructor(analytics: Analytics) {
     super(analytics);
   }
   send() {
     super.send();
-    console.log(`log ${this.id} into google analytics`);
+    console.log(`log ${this.getId()} into google analytics`);
   }
 }
 
+/**
+ * @class FacebookAnalytics
+ * @constructor
+ * @extends {AnalyticsDecorator}
+ */
 export class FacebookAnalytics extends AnalyticsDecorator {
   constructor(analytics: Analytics) {
     super(analytics);
   }
   send() {
     super.send();
-    console.log(`log ${this.id} into facebook analytics`);
+    console.log(`log ${this.getId()} into facebook analytics`);
   }
 }
